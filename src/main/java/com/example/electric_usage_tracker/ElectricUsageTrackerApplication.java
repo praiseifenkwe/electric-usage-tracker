c8748b8c79633c8b25836e57788f4ace0dce1e5c
Tycoonpraise <praiseifenkwe@gmail.com>
2025-04-06 15:18:09 +0100

    .env bug fix

--- src/main/java/com/example/electric_usage_tracker/ElectricUsageTrackerApplication.java
+++ src/main/java/com/example/electric_usage_tracker/ElectricUsageTrackerApplication.java
@@ -1,6 +1,5 @@
 package com.example.electric_usage_tracker;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElectricUsageTrackerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ElectricUsageTrackerApplication.class, args);
 	}
 
